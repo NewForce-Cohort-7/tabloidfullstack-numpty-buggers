c856054f70e59e13f97c860feece60eaeff45548
Kathleen Tyner <kathleentyner@gmail.com>
2023-07-18 15:41:07 -0400

    pushing form a different folder

---
+++ TabloidFullStack/TabloidFullStack/Controllers/TagController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TabloidFullStack.Repositories;
+
+namespace TabloidFullStack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+        private readonly ITagRepository _tagRepository;
+        public TagController(ITagRepository tagRepository)
+        {
+            _tagRepository = tagRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_tagRepository.GetAllTags());
+        }
+
+
+
+
+    }
+}
