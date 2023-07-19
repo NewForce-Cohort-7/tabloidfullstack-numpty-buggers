7f3cb2bd2586bd93a05b2e7cd6ae37d5ba6d6b7f
Arnold Rispress <arnold.rispress@gmail.com>
2023-07-19 10:17:38 -0400

    added a way to add categories

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/Managers/CategoryManager.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/Managers/CategoryManager.js
@@ -6,3 +6,13 @@ export const getAllCategories = () => {
     return fetch(`${apiUrl}/api/category`)
     .then((res) => res.json())
 };
+
+export const addCategory = (singleCategory) => {
+    return fetch (apiUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(singleCategory),
+    });
+};
--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/ApplicationViews.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/ApplicationViews.js
@@ -3,9 +3,10 @@ import { Route, Routes } from "react-router-dom";
 import Hello from "./Hello";
 import CategoryList from "./CategoryList";
 import Login from "./Login";
-
-
 import { TagList } from "./Tags/TagList"
+import CategoryForm from "./CategoryForm";
+
+
 export default function ApplicationViews() {
 
  return(
@@ -13,6 +14,7 @@ export default function ApplicationViews() {
         <Route path="/" element={<Hello />} />
         <Route path="/login" element={<Login />} />
         <Route path="/category" element={<CategoryList />} />
+        <Route path="/category/add" element={<CategoryForm />} />
         <Route path ="/tags" element ={<TagList />}/>
       </Routes>
    );
