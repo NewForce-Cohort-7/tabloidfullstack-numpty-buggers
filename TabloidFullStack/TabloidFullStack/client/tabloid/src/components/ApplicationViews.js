5fe545c165de36e3fdcff3137e6f604ce8a4bc08
Spencer Lott <spklott@gmail.com>
2023-07-19 09:45:54 -0400

    saving changes

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/ApplicationViews.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/ApplicationViews.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Hello from "./Hello";
-<<<<<<< HEAD
 import { PostList } from "./PostList";
-=======
 import CategoryList from "./CategoryList";
 import Login from "./Login";
 
->>>>>>> main
 
 import { TagList } from "./Tags/TagList"
 export default function ApplicationViews() {
@@ -15,12 +12,9 @@ export default function ApplicationViews() {
  return(
       <Routes>
         <Route path="/" element={<Hello />} />
-<<<<<<< HEAD
         <Route path="/posts" element={<PostList />} />
-=======
         <Route path="/login" element={<Login />} />
         <Route path="/category" element={<CategoryList />} />
->>>>>>> main
         <Route path ="/tags" element ={<TagList />}/>
       </Routes>
    );
