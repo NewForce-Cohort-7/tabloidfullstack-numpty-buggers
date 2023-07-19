d77feed9285b6b9b84e0a000b83a07284d4eee1a
Spencer Lott <spklott@gmail.com>
2023-07-19 15:05:00 -0400

    finished with post details

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/Posts/PostDetails.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/Posts/PostDetails.js
@@ -1,30 +1,36 @@
 import React, { useEffect, useState } from "react";
-import { Card, CardImg, CardBody } from "reactstrap";
+import { Card, CardImg, CardBody, CardTitle, CardText } from "reactstrap";
 import { Link, useParams } from "react-router-dom";
 import { getPostById } from "../../Managers/PostManager";
 
 export const PostDetails = () => {
-    const [post, setPost] = useState();
-    const { id } = useParams();
+  const [post, setPost] = useState();
+  const { id } = useParams();
 
-    useEffect(() => {
-        getPostById(id).then(setPost)
-    },[])
+  useEffect(() => {
+    getPostById(id).then(setPost);
+  }, []);
 
-    if (!post) {
-        return null;
-    }
+  if (!post) {
+    return null;
+  }
 
-  return (<>
-    <Card className="m-4">
-      <p>{post.title}</p>
-      <CardImg top src={post.imageLocation} alt={post.title} />
-      <CardBody>
-        {/* <p><strong>{post.title}</strong></p> */}
-        {/* <p>{post.caption}</p> */}
-      </CardBody>
+  return (
+    <Card>
+        <CardTitle>Title: <b>{post.title}</b></CardTitle>
+        <CardImg top src={post.ImageLocation} alt="iMAgE Is bROkeN..." />
+        <CardBody>
+            <CardText>{post.content}</CardText>
+            <CardText>
+                Posted on {post.createDateTime} by <b>{post?.userProfile?.displayName}</b>
+            </CardText>
+        </CardBody>
     </Card>
-    <div>oasdjfojadjf</div>
-  </>
   );
 };
+
+
+
+
+
+
